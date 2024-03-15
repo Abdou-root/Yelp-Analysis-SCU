c7074f9f239c5811798a268961c9fd298c6b063b
Abdessamad Grine <grineabdessamed2003@gmail.com>
2024-03-15 23:46:36 +0800

    Integrated most app features, final cleanup left

--- dbConnection.py
+++ dbConnection.py
@@ -1,6 +1,12 @@
 import pymysql.cursors
+from cachetools import cached, TTLCache
 
 # helper function to execute queries onto the streamlit web app
+
+# 1 hour cache of max 100 elements
+cache = TTLCache(maxsize=100, ttl=3600)
+
+@cached(cache)
 def run_query(query):
     connection = pymysql.connect(
         host='localhost',
